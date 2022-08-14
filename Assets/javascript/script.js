979af8b0c61edf0190a4966417c7ec7a373450df
Cayean <collinayala@gmail.com>
2022-08-14 17:29:02 -0500

    added initial bootsrap row's and columns

---
+++ Assets/javascript/script.js
@@ -0,0 +1,9 @@
+// add column to control the time slots
+// add row class to the column using Jquery
+
+// create funcitonality to save times to local storage 
+
+// use moment.js to make the past times grey
+
+//use moment to make the future times green and the closest hour interval red
+
