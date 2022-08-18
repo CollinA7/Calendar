08ad67910e5e5616d16f520bff9ecc2a6857ca59
Cayean <collinayala@gmail.com>
2022-08-17 23:11:19 -0500

    added JSON data to sent to local storage

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -3,8 +3,6 @@ var currentDay = $("#currentDay")
 var hour = $(".hour");
 var calendarEvent = {};
 
-
-
 console.log(moment().hour())
 
 currentDay.text(moment().format('dddd, LL'));
@@ -12,9 +10,9 @@ currentDay.text(moment().format('dddd, LL'));
 
 hour.each(function(){
     var workTime = parseInt($(this).next().attr("data-id"))
-    // console.log($(this).next().addClass("past"))
+    
     if (workTime < currentTime) {
-        // current time class gets removed
+        
         $(this).next().addClass("past");
         // then past time class get added
         console.log("past");
@@ -26,7 +24,7 @@ hour.each(function(){
     }
 
     else if (workTime === currentTime){
-        //future class is removed
+        
         $(this).next().addClass("present");
         //present class is added
         console.log("present");
@@ -34,23 +32,22 @@ hour.each(function(){
 })
 
 // calls click to edit event
-$(".description").on("click", function() {
-    var text = $(this).text().trim();
+$(".description").on("input", function() {
+    var text = $(this).val();
+    // console.log(text)
+
+    var calendarEvent = {text}
+    var textInput = JSON.stringify(calendarEvent);
+    console.log(calendarEvent)
     
-    var textInput = $("<textarea>").addClass("col-10 description").val(text);
 
-    $(this).replaceWith(textInput);
-
-    textInput.trigger("focus");
-    console.log(textInput.value)
-
-})
-
+});
 
 $(".saveBtn").click(function(){
+    
     console.log("saveBtn clicked");
     saveEvent();
-})
+});
 
 var saveEvent = function() {
     localStorage.setItem("calendarEvent", JSON.stringify(calendarEvent));
